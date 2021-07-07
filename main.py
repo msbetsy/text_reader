10612f2d7bbd054257fc1a16ab77c08cd8723af7
msbetsy <78367584+msbetsy@users.noreply.github.com>
2021-07-07 10:57:37 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,5 @@
+"""This module initializes the start of the program."""
+from ui import TextReaderInterface
+
+if __name__ == '__main__':
+    app = TextReaderInterface()
---
+++ ui.py
@@ -0,0 +1,5 @@
+"""The module is responsible for the graphical interface of all windows in application."""
+
+
+class TextReaderInterface:
+    pass
